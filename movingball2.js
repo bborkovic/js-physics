b0d91e9cedea9e1b054eba4d40cfb7fecef5e840
bborkovic <bborkovic@gmail.com>
2017-09-04 17:30:43 +0200

    changed

--- movingball2.js
+++ movingball2.js
@@ -1,26 +1,27 @@
 var Ball = Universe.Ball;
 var Vector = Universe.Vector;
 var Shapes = Universe.Shapes;
-velK = 0.99;
+velK = 0.9;
 
 Universe.setCanvas('canvas');
 
-var b = new Ball(20, "red", 30);
-b.setpos( new Vector(100,100) );
-b.setvel( new Vector(200,0) );
+// Add balls
+var b = new Ball(10, "red", 30);
+b.setpos( new Vector(100,350) );
+b.setvel( new Vector(100,0) );
 Universe.addObject(b,"ball");
 
 
-var sh = Shapes.createRectangle(new Vector(Universe.width/2,Universe.height+10) , Universe.width, 10);
+// Add shapes
+var sh = Shapes.createRectangle(new Vector(Universe.width/2,405) , Universe.width, 50);
 Universe.addObject(sh,"rect");
-sh = Shapes.createRectangle(new Vector(0-10,Universe.height/2) , 10, Universe.height);
-Universe.addObject(sh,"rect");
-sh = Shapes.createRectangle(new Vector(Universe.width+10,Universe.height/2) , 10, Universe.height);
-Universe.addObject(sh,"rect");
+// sh = Shapes.createRectangle(new Vector(0-10,Universe.height/2) , 10, Universe.height);
+// Universe.addObject(sh,"rect");
+// sh = Shapes.createRectangle(new Vector(Universe.width+10,Universe.height/2) , 10, Universe.height);
+// Universe.addObject(sh,"rect");
 
 
 
 
-Universe.setGravity();
-
-Universe.animate('canvas', 30);
+Universe.setGravity( new Vector(0,100) );
+Universe.animate('canvas', 20);
