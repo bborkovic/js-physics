60e7c4f4269e3322b4207dbc0841138ddf66ed0a
Boris Borkovic <bborkovic@gmail.com>
2017-09-06 06:15:29 +0200

    added spring

--- movingball2.js
+++ movingball2.js
@@ -2,7 +2,8 @@ var Ball = Universe.Ball;
 var Vector = Universe.Vector;
 var Shapes = Universe.Shapes;
 var Boundary = Universe.Boundary;
-velK = 0.9;
+var Spring = Universe.Spring;
+velK = 0.95;
 
 Universe.setCanvas('canvas');
 Universe.addBoundary( Boundary.createRect("top",0, velK) );
@@ -10,15 +11,28 @@ Universe.addBoundary( Boundary.createRect("left",0, velK) );
 Universe.addBoundary( Boundary.createRect("right",Universe.width, velK));
 Universe.addBoundary( Boundary.createRect("bottom",Universe.height, velK));
 
-var b = new Ball(20, "black", 30);
-b.setpos( new Vector( 300 , Universe.height-10 ) );
-b.setvel( new Vector(0,0) );
-Universe.addObject(b,"ball");
+// 
+var b1 = new Ball(20, "black", 30);
+b1.setpos( new Vector( 300 , Universe.height/2 ) );
+b1.setvel( new Vector(200,0) );
+Universe.addObject(b1,"ball");
+//
+var b2 = new Ball(20, "black", 30);
+b2.setpos( new Vector( 500 , Universe.height/2 ) );
+b2.setvel( new Vector(200,0) );
+Universe.addObject(b2,"ball");
 
-b = new Ball(20, "black", 30);
-b.setpos( new Vector( 300 , Universe.height/2 ) );
-b.setvel( new Vector(0,0) );
-Universe.addObject(b,"ball");
+// b1.connectTo(b2, s1);
+
+var s1 = Spring.create(10,10);
+var s2 = Spring.create(20,20);
+
+
+
+// b = new Ball(20, "black", 30);
+// b.setpos( new Vector( 300 , Universe.height/2 ) );
+// b.setvel( new Vector(0,0) );
+// Universe.addObject(b,"ball");
 
 // Add balls
 // var nrBalls = 15;
