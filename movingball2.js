94517f3ff930b2729b018e887137b18d7c2f5606
Boris Borkovic <bborkovic@gmail.com>
2017-09-09 17:33:23 +0200

    simulated hanging bridge

--- movingball2.js
+++ movingball2.js
@@ -3,7 +3,9 @@ var Vector = Universe.Vector;
 var Shapes = Universe.Shapes;
 var Boundary = Universe.Boundary;
 var Spring = Universe.Spring;
-velK = 0.95;
+var Line = Universe.Line;
+var velK = 0.9;
+var airDrag = 2;
 
 Universe.setCanvas('canvas');
 Universe.addBoundary( Boundary.createRect("top",0, velK) );
@@ -11,24 +13,57 @@ Universe.addBoundary( Boundary.createRect("left",0, velK) );
 Universe.addBoundary( Boundary.createRect("right",Universe.width, velK));
 Universe.addBoundary( Boundary.createRect("bottom",Universe.height, velK));
 
-// 
-var b1 = new Ball(20, "black", 30);
-b1.setpos( new Vector( 100 , Universe.height/2 ) );
-b1.setvel( new Vector(200,0) );
-Universe.addObject(b1,"ball");
-//
-var b2 = new Ball(20, "black", 30);
-b2.setpos( new Vector( 190 , Universe.height/2 ) );
-b2.setvel( new Vector(200,0) );
-Universe.addObject(b2,"ball");
+// var sh = Shapes.createRectangle( new Vector(800,Universe.height/2),40,300);
+// sh.transparent = true;
+// Universe.addObject(sh, "rect");
+// console.log(Universe.shapes);
 
-var s1 = Spring.create(10,100);
-var s2 = Spring.create(20,20);
 
-b1.connectTo(b2, s1, true);
+Universe.setGravity( new Vector(0,100) );
+var nrBalls = 40;
+var ball_radius = 10;
+var ball_mass = 2;
+var hor_begin = Universe.width*1/10;
+var hor_end = Universe.width*9/10;
+var distance = ( hor_end - hor_begin )/nrBalls;
+console.log(distance);
 
-console.log( "Force is");
-console.log(b1.getSpringPullback());
+var balls = [];
+for( var i = 0; i <= nrBalls; i++){
+   var ball = new Ball(ball_radius, "black", ball_mass);
+   balls.push(ball);
+   ball.setpos( new Vector( hor_begin + (i*distance), Universe.height/20 ) );
+   Universe.addObject(ball,"ball");
+}
+
+balls[0].fixed = true;
+balls[nrBalls].fixed = true;
+
+var heavy_ball1 = new Ball(ball_radius*3, "black", ball_mass*20);
+heavy_ball1.setpos( new Vector( balls[Math.floor(nrBalls/3)].x , balls[Math.floor(nrBalls/3)].y + 3*distance)) ;
+Universe.addObject(heavy_ball1, "ball");
+
+
+var s = Spring.create(1000, distance); // (k, L, dumping)
+var s2 = Spring.create(1000, distance*3); // (k, L, dumping)
+for( var i = 0; i < nrBalls; i++ ){
+   balls[i].connectTo( balls[i+1], s, true );
+}
+balls[ Math.floor(nrBalls*1/3)].connectTo( heavy_ball1, s2, true );
+
+
+
+
+
+
+
+// var top_left  = Spring.create(1000, 200); // (k, L, dumping)
+// var left_right = Spring.create(1000, 200); // (k, L, dumping)
+
+// ball_top.connectTo(ball_right, s, true);
+// ball_top.connectTo(ball_left, s, true);
+// ball_left.connectTo(ball_right, s, true);
+
 
 // b = new Ball(20, "black", 30);
 // b.setpos( new Vector( 300 , Universe.height/2 ) );
@@ -45,5 +80,5 @@ console.log(b1.getSpringPullback());
 // }
 
 
-Universe.setGravity( new Vector(0,1000) );
-// Universe.animate('canvas', 5);
+
+Universe.animate('canvas', 100);
